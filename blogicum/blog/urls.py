1ddb8be64e2f6a426470c5e85a5bcc074be67f8f
Mark-Petrov <marikp20@gmail.com>
2025-05-24 15:34:04 +0300

    Проект до запуска тестов

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.post_detail, name='detail'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category')
+]
