2bcc7fed4b09c4ad03b25fd95088c0ac57b775bd
Mark-Petrov <marikp20@gmail.com>
2025-05-24 15:45:51 +0300

    Проект после правок 1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -51,10 +51,12 @@ def index(request):
     }
     return render(request, template, context)
 
+
 def post_detail(request, id):
     template = 'blog/detail.html'
     context = {'post': posts[id]}
     return render(request, template, context)
+
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
